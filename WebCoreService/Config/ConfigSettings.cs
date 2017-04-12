daa023edc663b3dfb43956b877b7ac2e55e86ef9
ppolyzos <ppolyzos@gmail.com>
2017-04-12 22:45:55 +0300

    feat(web): initial implementation with config settings

---
+++ WebCoreService/Config/ConfigSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Fabric;
+using System.Fabric.Description;
+
+namespace WebCoreService.Config
+{
+    public class ConfigSettings
+    {
+        public string StatelessServiceName { get; private set; }
+        public string StatefulServiceName { get; private set; }
+
+        public ConfigSettings(StatelessServiceContext context)
+        {
+            context.CodePackageActivationContext.ConfigurationPackageModifiedEvent += this.CodePackageActivationContext_ConfigurationPackageModifiedEvent;
+            this.UpdateConfigSettings(context.CodePackageActivationContext.GetConfigurationPackageObject("Config").Settings);
+        }
+
+        private void CodePackageActivationContext_ConfigurationPackageModifiedEvent(object sender, PackageModifiedEventArgs<ConfigurationPackage> e)
+        {
+            this.UpdateConfigSettings(e.NewPackage.Settings);
+        }
+
+        private void UpdateConfigSettings(ConfigurationSettings settings)
+        {
+            ConfigurationSection section = settings.Sections["MyConfigSection"];
+            this.StatelessServiceName = section.Parameters["StatelessServiceName"].Value;
+            this.StatefulServiceName = section.Parameters["StatefulServiceName"].Value;
+        }
+    }
+}
